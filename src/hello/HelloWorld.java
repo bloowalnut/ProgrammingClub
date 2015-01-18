3404efca475e1b73da42a3fd5c2d6251ec508e7c
nicyoloswag <nlabadan18@deerfield.edu>
2015-01-18 16:33:53 -0500

    added aids

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -21,7 +21,7 @@ public class HelloWorld {
 	//For one line comment
 	public static void main(String[] args){
 		System.out.println("Hello, world!");
-		System.out.println("Hahaha!");
+		System.out.println("YOU HAVE SO MUCH AIDS!");
 		int x = 3; //declares and defines x
 		System.out.println(x); //prints x
 		long peopleOfTheWorld = 7000000000L;
